b7783e219be971e75e3d4e0412522af771947d60
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-04 10:53:40 +0000

    create controller

--- routes/projetos.js
+++ routes/projetos.js
@@ -1,14 +1,8 @@
 const { Router } = require("express");
+const { getProjetos } = require("../controllers/projetos");
 const router = Router();
 
-router.get('/', (req, res) => {
-    try {
-        res.send("Hello, world!");
-    } catch (error) {
-        res.status(500);
-        res.send(error.message);
-    }
-});
+router.get('/', getProjetos());
 
 router.post('/', (req, res) => {
     res.send("Você fez uma requisição do tipo POST")
