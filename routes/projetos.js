49f5ec7a997c4dc2a5b1020cb2ddda183a63729c
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-04 00:22:04 +0000

    created routers post, patch, delete

--- routes/projetos.js
+++ routes/projetos.js
@@ -2,7 +2,24 @@ const { Router } = require("express");
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send("Hello, world!");
+    try {
+        res.send("Hello, world!");
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+});
+
+router.post('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo POST")
+});
+
+router.patch('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo PATCH")
+});
+
+router.delete('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo DELETE")
 });
 
 module.exports = router;
