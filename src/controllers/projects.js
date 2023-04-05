766a613ec964b545bdea4dc7cc26472187a1bb6d
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-04-05 19:27:11 -0300

    Add: error handling

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -5,18 +5,24 @@ function getAll(req, res) {
         const projects = getAllProjects();
         res.send(projects);
     } catch (error) {
-        //res.status(500);
-        //res.send(error.message);
+        res.status(500);
+        res.send(error.message);
     }
 }
 
 function getProject(req, res) {
     try {
         const id = req.params.id;
-        const project = getProjectById(id);
-        res.send(project);
+        if (id && Number(id)) {
+            const project = getProjectById(id);
+            res.send(project);
+        } else {
+            res.status(422);
+            res.send("Invalid Id");
+        }
     } catch (error) {
-
+        res.status(500);
+        res.send(erro.message);
     }
 }
 
@@ -27,33 +33,46 @@ function postProject(req, res) {
         res.status(201);
         res.send("success");
     } catch (error) {
-
+        res.status(500);
+        res.send(error.message);
     }
 }
 
 function patchProject(req, res) {
     try {
         const id = req.params.id;
-        const body = req.body;
-        editProject(body, id);
-        res.send("success");
+        if (id && Number(id)) {
+            const body = req.body;
+            editProject(body, id);
+            res.send("success");
+        } else {
+            res.status(422);
+            res.send("Invalid Id");
+        }
     } catch (error) {
-
+        res.status(500);
+        res.send(error.message);
     }
 }
 
 function deleteProject(req, res) {
     try {
         const id = req.params.id;
-        deleteProjectById(id);
-        res.send("success");
+        if (id && Number(id)) {
+            deleteProjectById(id);
+            res.send("success");
+        } else {
+            res.status(422);
+            res.send("Invalid Id");
+        }
     } catch (error) {
-        
+        res.status(500);
+        res.send(error.message);
     }
 }
 
 module.exports = {
-	getAll,
+    getAll,
     getProject,
     postProject,
     patchProject,
