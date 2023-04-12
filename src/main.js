c669c5bf8d4d769568073dbddc5461b932fb92e9
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-04-11 21:03:58 -0300

    add: CRUD app

--- src/main.js
+++ src/main.js
@@ -1,12 +1,51 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 app.use(express.json());
-const port = 8000;
+const projects = [
+    {
+        id: 1,
+        "title": "calculadora"
+    },
+    {
+        id: 2,
+        "title": "cronometro"
+    }
+];
 
-const projects = require("./routes/projects");
-
-app.use("/projects", projects);
-
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, world!');
 });
+
+app.get('/projects', (req, res) => {
+    res.status(200).send(projects);
+});
+
+app.get('/projects/:id', (req, res) => {
+    let index = getProject(req.params.id);
+    res.json(projects[index]);
+});
+
+app.post('/projects', (req, res) => {
+    projects.push(req.body);
+    res.status(201).send('project insert with success');
+});
+
+app.put('/projects/:id', (req, res) => {
+    let index = getProject(req.params.id);
+    projects[index].title = req.body.title;
+    res.json(projects);
+});
+
+app.delete('/projects/:id', (req, res) => {
+    let {id} = req.params;
+    let index = getProject(id);
+    projects.splice(index, 1);
+    res.send(`project ${id} deleted with sucess`);
+});
+
+function getProject(id) {
+    return projects.findIndex(project => project.id == id);
+}
+
+export default app;
