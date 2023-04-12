7d038c73fdf3b7e71dc943ebd00531619d3272a7
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-04-11 22:07:27 -0300

    create projectSchema

---
+++ src/models/Project.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+    id:     { type: String },
+    title:  { type: String, required: true },
+    url:    { type: String, required: true },
+});
+
+const projects = mongoose.model('projects', projectSchema);
+
+export default projects;
