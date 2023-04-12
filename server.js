eac609bcdcfebf05a64b88e89a8482f65953fabd
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-04-11 21:03:11 -0300

    updating server

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-const http = require('http');
-const port = 3000;
+import app from './src/main.js';
 
-const routes = {
-    '/': 'Hello, World!',
-    '/projects': 'Dive into the projects',
-    '/mini-projects': 'Come into the mini projects'
-}
+const port = process.env.PORT  || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(routes[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`server listening on port: ${port}`);
 });
