f1d35d5a4fc676e46fd36044c782e9ad7cb691e1
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-15 14:07:03 +0000

    app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import projects from "./models/Project.js";
 import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Connection error'));
@@ -10,34 +9,6 @@ db.once("open", () => {
 
 const app = express();
 app.use(express.json());
-
 routes(app);
-
-app.get('/projects/:id', (req, res) => {
-    let index = getProject(req.params.id);
-    res.json(projects[index]);
-});
-
-app.post('/projects', (req, res) => {
-    projects.push(req.body);
-    res.status(201).send('project insert with success');
-});
-
-app.put('/projects/:id', (req, res) => {
-    let index = getProject(req.params.id);
-    projects[index].title = req.body.title;
-    res.json(projects);
-});
-
-app.delete('/projects/:id', (req, res) => {
-    let {id} = req.params;
-    let index = getProject(id);
-    projects.splice(index, 1);
-    res.send(`project ${id} deleted with sucess`);
-});
-
-function getProject(id) {
-    return projects.findIndex(project => project.id == id);
-}
 
 export default app;
