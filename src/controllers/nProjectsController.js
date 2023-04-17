692ec6c6637c390f80ac2013609bb407fb3583f9
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-04-17 00:57:16 -0300

    Refact: controller

--- src/controllers/nProjectsController.js
+++ src/controllers/nProjectsController.js
@@ -2,59 +2,58 @@ import nProjects from "../models/NProjects.js";
 
 class NProjectController {
 
-    static getAllProjects = (req, res) => {
-        nProjects.find((err, projects) => {
-            res.status(200).json(projects);
-        });
+    static getAllProjects = async (req, res) => {
+        try {
+            const result = await nProjects.find();
+            res.status(200).json(result);
+
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - Internal error server.` });
+        }
     };
 
-    static getProjectById = (req, res) => {
-        const id = req.params.id;
+    static getProjectById = async (req, res) => {
+        try {
+            const id = req.params.id;
+            const result = await nProjects.findById(id);
+            res.status(200).send(result);
 
-        nProjects.findById(id, (err, projects) => {
-            if (err) {
-                res.status(400).send({ message: `${err.message} - Id not found.` });
-            } else {
-                res.status(200).send(projects);
-            }
-        });
+        } catch (error) {
+            res.status(400).send({ message: `${error.message} - Id not found.` });
+        }
     };
 
-    static postProject = (req, res) => {
-        let project = new nProjects(req.body);
+    static postProject = async (req, res) => {
+        try {
+            let project = new nProjects(req.body);
+            const result = await project.save();
+            res.status(201).send(result.toJSON());
 
-        project.save((err) => {
-            if (err) {
-                res.status(500).send({ message: `Error: ${err.message} - failed to register project` });
-            } else {
-                res.status(201).send(project.toJSON());
-            }
-        });
+        } catch (error) {
+            res.status(500).send({ message: `Error: ${error.message} - failed to register project.` });
+        }
     };
 
-    static updateProject = (req, res) => {
-        const id = req.params.id;
-
-        nProjects.findByIdAndUpdate(id, { $set: req.body }, (err) => {
-            if (!err) {
-                res.status(200).send({ message: "Project Update with success" });
-            } else {
-                res.status(500).send({ message: err.message });
-            }
-        });
-
+    static updateProject = async (req, res) => {
+        try {
+            const id = req.params.id;
+            await nProjects.findByIdAndUpdate(id, { $set: req.body });
+            res.status(200).send({ message: "Project Updated with success." });
+            
+        } catch (error) {
+            res.status(500).send({ message: error.message });
+        }
     };
 
-    static deleteProject = (req, res) => {
-        const id = req.params.id;
+    static deleteProject = async (req, res) => {
+        try {
+            const id = req.params.id;
+            await nProjects.findByIdAndDelete(id); 
+            res.status(200).send({ message: "Project removed with success." });
 
-        nProjects.findByIdAndDelete(id, (err) => {
-            if (!err) {
-                res.status(200).send({ messa: "Project removed with success." });
-            } else {
-                res.status(200).send({ message: err.message });
-            }
-        });
+        } catch (error) {
+            res.status(500).send({ message: error.message });
+        }
     };
 
 }
