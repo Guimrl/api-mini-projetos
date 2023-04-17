c30f97b49d84984202e95262a1070d3ebf0d5d31
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-04-17 00:57:31 -0300

    refact: controller

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -2,71 +2,70 @@ import projects from "../models/Project.js";
 
 class ProjectController {
 
-    static getAllProjects = (req, res) => {
-        projects.find((err, projects) => {
-            res.status(200).json(projects);
-        });
+    static getAllProjects = async (req, res) => {
+        try {
+            const result = await projects.find();
+            res.status(200).json(result);
+            
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - Internal error server.` });
+        }
+        
     };
 
-    static getProjectById = (req, res) => {
-        const id = req.params.id;
-
-        projects.findById(id, (err, projects) => {
-            if (err) {
-                res.status(400).send({ message: `${err.message} - Id not found.` });
-            } else {
-                res.status(200).send(projects);
-            }
-        });
+    static getProjectById = async (req, res) => {
+        try {
+            const id = req.params.id;
+            const result = await projects.findById(id);
+            res.status(200).send(result);
+            
+        } catch (error) {
+            res.status(400).send({ message: `${error.message} - Id not found.` });
+        }
     };
 
-    static getProjectByName = (req, res) => {
-        const title = req.query.title;
+    static getProjectByName = async (req, res) => {
+        try {
+            const title = req.query.title;
+            const result = await projects.find({ "title": title });
+            res.status(200).send(result);
 
-        projects.find({ "title": title }, {}, (err, projects) => {
-            if (err) {
-                res.status(400).send({ message: `${err.message} - Name not found.` });
-            } else {
-                res.status(200).send(projects);
-            }
-        });
+        } catch (error) {
+            res.status(400).send({ message: `${error.message} - Name not found.` });
+        }
     };
 
-    static postProject = (req, res) => {
-        let project = new projects(req.body);
+    static postProject = async (req, res) => {
+        try {
+            let project = new projects(req.body);
+            const result = await project.save();
+            res.status(201).send(result.toJSON());
 
-        project.save((err) => {
-            if (err) {
-                res.status(500).send({ message: `Error: ${err.message} - failed to register project` });
-            } else {
-                res.status(201).send(project.toJSON());
-            }
-        });
+        } catch (error) {
+            res.status(500).send({ message: `Error: ${error.message} - failed to register project.` });
+        }
     };
 
-    static updateProject = (req, res) => {
-        const id = req.params.id;
+    static updateProject = async (req, res) => {
+        try {
+            const id = req.params.id;
+            await projects.findByIdAndUpdate(id, { $set: req.body });
+            res.status(200).send({ message: "Project Updated with success." });
 
-        projects.findByIdAndUpdate(id, { $set: req.body }, (err) => {
-            if (!err) {
-                res.status(200).send({ message: "Project Update with success" });
-            } else {
-                res.status(500).send({ message: err.message });
-            }
-        });
-
+        } catch (error) {
+            res.status(500).send({ message: error.message });
+        }
     };
 
-    static deleteProject = (req, res) => {
-        const id = req.params.id;
+    static deleteProject = async (req, res) => {
+        try {
+            const id = req.params.id;
+            await projects.findByIdAndDelete(id);
+            res.status(200).send({ message: "Project removed with success." });
 
-        projects.findByIdAndDelete(id, (err) => {
-            if (!err) {
-                res.status(200).send({ messa: "Project removed with success." });
-            } else {
-                res.status(200).send({ message: err.message });
-            }
-        });
+        } catch (error) {
+            res.status(200).send({ message: error.message });
+        }
     };
 
 }
