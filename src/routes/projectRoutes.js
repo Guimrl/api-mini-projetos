491c9bf8388e3078a54d0987117bedbbc4aac2e7
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-21 15:11:45 +0000

    fix: module

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import ProjectController from "../controllers/projectController.js";
+import ProjectController from "../controllers/ProjectController.js";
 
 const router = express.Router();
 
