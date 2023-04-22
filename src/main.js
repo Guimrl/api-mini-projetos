32bb99a6662518cfd202e47fb17ca7155a1b3dee
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-22 21:09:03 +0000

    use: cors

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 
@@ -9,6 +10,14 @@ db.once("open", () => {
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+    //console.log("on middleware!");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    app.use(cors());
+    next();
+});
+
 routes(app);
 
 export default app;
